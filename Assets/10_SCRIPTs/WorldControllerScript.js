3ffb4269e560fe8b4b396b40307d422a3b1a0dcc
Reto Spoerri <rspoerri@nouser.org>
2017-09-25 16:51:35 +0200

    update stuff

--- Assets/10_SCRIPTs/WorldControllerScript.js
+++ Assets/10_SCRIPTs/WorldControllerScript.js
@@ -1,3 +1,5 @@
+#pragma strict
+
 /* funktion zum erstellen eines cubes, mit einer position */
 function CreateCube (pos : Vector3) {
 	var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
@@ -9,60 +11,95 @@ function CreateCube (pos : Vector3) {
 function Start () {
 	// startpunkt der kollision
 	var raySource : Vector3;
-	// richtung des rays
-	var rayDirection : Vector3 = Vector3.down;
-	// speicherplatz für das resultat der kollision
-	var rayHit : RaycastHit;
 	
-	// grösse des platzieten objektes
-	var cubeScale : float = 5;
+	// gruesse des platzieten objektes
+	var cubeScale : float = 10;
 	// minimaler abstand zwischen den objekten
 	var cubeMinSpacing : float = 1;
-	// range für die platzierung der objekte (-placingRange .. +placingRange)
-	var placingRange : float = 400;
-	// zähler für anzahl platzierter objekte
+	// range fuer die platzierung der objekte (-placingRange .. +placingRange)
+	var placingRange : float = 250;
+	// zuehler fuer anzahl platzierter objekte
 	var cubeCount : int = 0;
 	
 	// 1000 mal
 	for (var i=0; i<300; i++) {
-		// bestimme ein zentrum für das objekt, zufällig in x und y position (-placingRange .. +placingRange)
+		// bestimme ein zentrum fuer das objekt, zufaellig in x und y position (-placingRange .. +placingRange)
 		raySource = Vector3(Random.Range(-placingRange, placingRange), 100, Random.Range(-placingRange, placingRange));
-		// ist das resultat innerhalb des platzer wo fahrzeuge positioniert werden
+		// ist das resultat innerhalb des platzes wo das fahrzeug fÃ¤hrt
 		if ((-50 < raySource.x && raySource.x < 50) && (-50 < raySource.z && raySource.z < 50)) {
-			// führe die platzierung nicht aus
+			// fuehre die platzierung nicht aus
 		} else {
-			// prüfe ob auf 4 richtungen ob das objekt auf das terrain oder ein anderes objekt trifft
-			// zentrum + (grösse des objektes+abstand des objektes in x und z koordinate) als startpunkt des rays
-			Physics.Raycast(raySource+Vector3(cubeScale+cubeMinSpacing,0,cubeScale+cubeMinSpacing), rayDirection, rayHit);
-			// mach nur weiter wenn wir auf das terrain treffen
-			if (rayHit.collider.gameObject.name == "Terrain") {
-				// wiederhole den schritt für
-				// -x, 0, z richtung
-				Physics.Raycast(raySource+Vector3(-cubeScale+cubeMinSpacing,0,cubeScale+cubeMinSpacing), rayDirection, rayHit);
-				if (rayHit.collider.gameObject.name == "Terrain") {
-					// wiederhole den schritt für
-					// x, 0, -z richtung
-					Physics.Raycast(raySource+Vector3(cubeScale+cubeMinSpacing,0,-cubeScale+cubeMinSpacing), rayDirection, rayHit);
-					if (rayHit.collider.gameObject.name == "Terrain") {
-						// wiederhole den schritt für
-						// -x, 0, -z richtung
-						Physics.Raycast(raySource+Vector3(-cubeScale+cubeMinSpacing,0,-cubeScale+cubeMinSpacing), rayDirection, rayHit);
-						if (rayHit.collider.gameObject.name == "Terrain") {
-							// mache einen ray um die höhe im zentrum zu bestimmen
-							Physics.Raycast(raySource, rayDirection, rayHit);
-							if (rayHit.collider.gameObject.name == "Terrain") {
-								// platziere den cube an dieser position
-								CreateCube(rayHit.point);
-								cubeCount += 1;
-							}
-						}
-					}
-				}
+			// pruefe ob auf 4 richtungen ob das objekt auf das terrain oder ein anderes objekt trifft
+			// zentrum + (gruesse des objektes+abstand des objektes in x und z koordinate) als startpunkt des rays
+			if (	PosCheck(raySource+Vector3(  cubeScale+cubeMinSpacing, 0,  cubeScale+cubeMinSpacing))
+				&&	PosCheck(raySource+Vector3( -cubeScale+cubeMinSpacing, 0,  cubeScale+cubeMinSpacing))
+				&&	PosCheck(raySource+Vector3(  cubeScale+cubeMinSpacing, 0, -cubeScale+cubeMinSpacing))
+				&&	PosCheck(raySource+Vector3( -cubeScale+cubeMinSpacing, 0, -cubeScale+cubeMinSpacing))) {
+					var pos : Vector3 = GetHeight(raySource);
+					// platziere den cube an dieser position
+					CreateCube(pos);
+					cubeCount += 1;
 			}
+
+			// // pruefe ob auf 4 richtungen ob das objekt auf das terrain oder ein anderes objekt trifft
+			// // zentrum + (gruesse des objektes+abstand des objektes in x und z koordinate) als startpunkt des rays
+			// Physics.Raycast(raySource+Vector3(cubeScale+cubeMinSpacing,0,cubeScale+cubeMinSpacing), rayDirection, rayHit);
+			// // mach nur weiter wenn wir auf das terrain treffen
+			// if (rayHit.collider.gameObject.name == "Terrain") {
+			// 	// wiederhole den schritt fuer
+			// 	// -x, 0, z richtung
+			// 	Physics.Raycast(raySource+Vector3(-cubeScale+cubeMinSpacing,0,cubeScale+cubeMinSpacing), rayDirection, rayHit);
+			// 	if (rayHit.collider.gameObject.name == "Terrain") {
+			// 		// wiederhole den schritt fuer
+			// 		// x, 0, -z richtung
+			// 		Physics.Raycast(raySource+Vector3(cubeScale+cubeMinSpacing,0,-cubeScale+cubeMinSpacing), rayDirection, rayHit);
+			// 		if (rayHit.collider.gameObject.name == "Terrain") {
+			// 			// wiederhole den schritt fuer
+			// 			// -x, 0, -z richtung
+			// 			Physics.Raycast(raySource+Vector3(-cubeScale+cubeMinSpacing,0,-cubeScale+cubeMinSpacing), rayDirection, rayHit);
+			// 			if (rayHit.collider.gameObject.name == "Terrain") {
+			// 				// mache einen ray um die hoehe im zentrum zu bestimmen
+			// 				Physics.Raycast(raySource, rayDirection, rayHit);
+			// 				if (rayHit.collider.gameObject.name == "Terrain") {
+			// 					// platziere den cube an dieser position
+			// 					CreateCube(rayHit.point);
+			// 					cubeCount += 1;
+			// 				}
+			// 			}
+			// 		}
+			// 	}
+			// }
 		}
 	}
 	print("I: WorldControllerScript.Start: placed "+cubeCount+" Cubes");
 	
+}
+
+function PosCheck (sourcePos : Vector3) : boolean {
+	// richtung des rays
+	var rayDirection : Vector3 = Vector3.down;
+	// speicherplatz fuer das resultat der kollision
+	var rayHit : RaycastHit;
+
+	Physics.Raycast(sourcePos, rayDirection, rayHit);
+	if (rayHit.collider.gameObject.name == "Terrain") {
+		return true;
+	}
+	return false;
+}
+
+function GetHeight (sourcePos : Vector3) : Vector3 {
+	// richtung des rays
+	var rayDirection : Vector3 = Vector3.down;
+	// speicherplatz fuer das resultat der kollision
+	var rayHit : RaycastHit;
+
+	Physics.Raycast(sourcePos, rayDirection, rayHit);
+	if (rayHit.collider.gameObject.name == "Terrain") {
+		return rayHit.point;
+	}
+	// gib einen wert zurÃ¼ck der nicht stoert, besser ware es sauber abzufangen
+	return new Vector3(0,100,0);
 }
 
 function Update () {
