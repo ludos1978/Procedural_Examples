27f8f8293aa20ee98896eac04e5e944dfad6da32
Reto Spoerri <rspoerri@nouser.org>
2017-09-27 15:14:53 +0200

    add unity navmesh example

---
+++ Assets/10_SCRIPTs/LinearCharacterMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearCharacterMovement : MonoBehaviour {
+
+	public Vector3[] wayPoints;
+	public int wayPointIndex = 0;
+
+	public Transform target;
+	public float movementSpeed = 3;
+
+	// Update is called once per frame
+	void Update () {
+		if ((target.transform.position - transform.position).magnitude < 10) {
+			MoveTo(target.transform.position);
+		} else {
+			if (MoveTo(wayPoints[wayPointIndex])) {
+				// arrived at position
+				wayPointIndex = (wayPointIndex + 1) % wayPoints.Length;
+			} else {
+				// goto current target
+			}
+		}
+	}
+	bool MoveTo (Vector3 targetPos) {
+		Vector3 targetOffset = targetPos - transform.position;
+		targetOffset = targetOffset.normalized;
+		transform.position += targetOffset * Time.deltaTime * movementSpeed;
+		if ((targetPos - transform.position).magnitude < 0.1f) {
+			return true;
+		}
+		return false;
+	}
+}
